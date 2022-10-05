cd9871367c062914aad93b978b48dbd456844801
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-06 01:13:24 +0900

    Initial Commit

---
+++ SampleNeumorphism/SampleNeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleNeumorphismApp.swift
+//  SampleNeumorphism
+//
+//  Created by mtanaka on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleNeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
