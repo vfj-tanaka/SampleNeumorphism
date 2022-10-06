74f157c822d1ff4ee6176f704fa970d62b9448aa
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-06 18:09:10 +0900

    サンプル。

--- SampleNeumorphism/ContentView.swift
+++ SampleNeumorphism/ContentView.swift
@@ -9,15 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var text1 = ""
+    private let fruits = ["りんご", "オレンジ", "バナナ"]
+    
     var body: some View {
         
         NavigationStack {
+            
             ZStack {
+                
                 Color.neuBackground
                     .ignoresSafeArea()
-                VStack {
-                    Image(systemName: "forward.fill")
-                        .neumorphism(padding: 30)
+                
+                VStack(alignment: .center, spacing: 50) {
+                    
+                    HStack(spacing: 20) {
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "apple.logo")
+                        }
+                        .neumorphism(padding: 30, cornerRadius: 40)
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "apple.logo")
+                        }
+                        .neumorphism(padding: 30, cornerRadius: 20)
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "apple.logo")
+                        }
+                        .neumorphism(padding: 30, cornerRadius: 5)
+                    }
+                    
+                    TextField("ニューモフィズム〜", text: $text1)
+                        .neumorphism(padding: 20, cornerRadius: 10)
                 }
                 .padding()
             }
--- SampleNeumorphism/View+.swift
+++ SampleNeumorphism/View+.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 extension View {
-    func neumorphism(padding: CGFloat)->some View {
-        self.modifier(NeumorphismModifier(padding: padding))
+    func neumorphism(padding: CGFloat, cornerRadius: CGFloat)->some View {
+        self.modifier(NeumorphismModifier(padding: padding, cornerRadius: cornerRadius))
     }
 }
 
 struct NeumorphismModifier: ViewModifier {
     var padding: CGFloat = 5
+    var cornerRadius: CGFloat = 10
     func body(content:Content) -> some View {
         ZStack(content: {
             content
                 .padding(padding)
                 .background(
-                    RoundedRectangle(cornerRadius: 10)
+                    RoundedRectangle(cornerRadius: cornerRadius)
                     .fill(Color.neuBackground)
                 )
-                .shadow(color: .dropShadow, radius: 11, x: 8, y: 8)
-                .shadow(color: .dropLight, radius: 11, x: -8, y: -8)
+                .shadow(color: .dropShadow, radius: 10, x: 6, y: 6)
+                .shadow(color: .dropLight, radius: 10, x: -6, y: -6)
                 .foregroundColor(.primary)
         })
     }
